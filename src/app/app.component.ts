a4e33db2581ede292c1fd368473113863de13a71
leila <leyla.es@gmail.com>
2023-11-24 06:32:12 -0800

    ui2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -6,9 +6,10 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  {
   constructor(private router:Router){}
   title = 'project';
+ 
   new(){
     this.router.navigateByUrl('/register/regist');
   }
